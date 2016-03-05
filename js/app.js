c9988eee8abf1d7b3e3bdaac356a4db2b084cfea
Kirsten Lambertsen <kirsten@pseudolusmedia.com>
2016-03-04 20:58:10 -0500

    Cleanup
    Remove api keys and cleanup unnecessary comments

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
 // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('xpnk', [
 'ionic',
 'ngCordova',
@@ -30,7 +25,7 @@ angular.module('xpnk', [
     }
 
     //TODO: check for mobile device and only run if on mobile
-    OAuth.initialize('X2__GNhzcc6fng7PNt9t7euQwpQ');
+    OAuth.initialize('');
 
   })
 
@@ -40,9 +35,9 @@ angular.module('xpnk', [
   // Identify app
   $ionicAppProvider.identify({
     // The App ID (from apps.ionic.io) for the server
-    app_id: '39fb755e',
+    app_id: '',
     // The public API key all services will use for this app
-    api_key: '88a837d36b218f134e504129f156dca86a60c74bff62ed9f',
+    api_key: '',
     // Set the app to use development pushes
     dev_push: true
   });
@@ -51,8 +46,6 @@ angular.module('xpnk', [
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
